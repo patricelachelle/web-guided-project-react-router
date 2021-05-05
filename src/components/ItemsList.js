57fbc2a543cbf44d5d8642416c87e51cdd31f9dd
Ben Schinn <ben.schinn@mx.com>
2021-05-04 21:27:46 -0500

    solution

--- src/components/ItemsList.js
+++ src/components/ItemsList.js
@@ -1,8 +1,10 @@
 import React from 'react'
 // We'll need a Link and the useLocation hook from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 export default function ItemsList(props) {
   const { items } = props
+  const location = useLocation()
 
   // We'll grab the current URL using the hook
 
@@ -14,14 +16,15 @@ export default function ItemsList(props) {
           key={item.id}
         >
           {/* 👉 STEP 6 - Link starts, navigates us from <current url> to <current url>/<id of the item> */}
-          <img
-            className='items-list-image'
-            src={item.imageUrl}
-            alt={item.name}
-          />
-          <p>{item.name}</p>
-          {/* Link ends */}
-
+          <Link to={`${location.pathname}/${item.id}`}>
+            <img
+              className='items-list-image'
+              src={item.imageUrl}
+              alt={item.name}
+            />
+            <p>{item.name}</p>
+            {/* Link ends */}
+          </Link>
           <p>${item.price}</p>
         </div>
       ))}
